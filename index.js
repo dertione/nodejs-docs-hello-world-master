9ea4c444e7d6dde0df68bc4022e59aa1822c54a4
dertione <mormotrobien@gmail.com>
2018-06-04 11:53:52 +0200

    post listener

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-var http = require('http');
+var app = require('http').createServer(handler);
+var url = require('url') ;
 
-var server = http.createServer(function(request, response) {
+var statusCode = 200;
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+app.listen(9000);
 
-});
+function handler (req, res) {
+  var data = '';
 
-var port = process.env.PORT || 1337;
-server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+    console.log(req.url);
+    console.log(url.parse(req.url,true));
+
+	  var queryObject = url.parse(req.url,true).query;
+
+  if (req.method == "POST") {
+    req.on('data', function(chunk) {
+      data += chunk;
+    });
+
+    req.on('end', function() {
+      console.log('Received body data:');
+      console.log(data.toString());
+
+    });
+  }
+
+  console.log("Query strings: " + JSON.stringify(queryObject));
+
+  res.writeHead(statusCode, {'Content-Type': 'text/plain'});
+  res.end();
+}
+
+console.log("Listening to port 8080");
+console.log("Returning status code " + statusCode.toString());
