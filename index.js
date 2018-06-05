d61d299624f2feb9b36f17bc40a0a71f7bab66b0
dertione <mormotrobien@gmail.com>
2018-06-05 14:20:47 +0200

    backup

--- index.js
+++ index.js
@@ -1,56 +1,26 @@
-/*
- * Copyright (C) 2016 Orange
- *
- * This software is distributed under the terms and conditions of the 'BSD-3-Clause'
- * license which can be found in the file 'LICENSE.txt' in this package distribution
- * or at 'https://opensource.org/licenses/BSD-3-Clause'.
- */
 var fs = require("fs");
-var mqtt = require('mqtt')
-const url = "mqtt://liveobjects.orange-business.com:1883"
-const apiKey = "dbef594c58cc4fcca07284d1accb9d2d"
+var app = require('http').createServer(handler);
+var statusCode = 200;
 
+app.listen(443);
 
-/** Subscription for one specific device (pub sub) **/
-// const mqttTopic = "router/~event/v1/data/new/urn/lora/0123456789ABCDEF/#"
+function handler (req, res) {
+  var data = '';
 
-/** Subscription for all devices (pub sub) **/
-const mqttTopic = "router/~event/v1/data/new/urn/lora/#"
+  if (req.method == "POST") {
+    req.on('data', function(chunk) {
+      data += chunk;
+    });
 
-/** Subscription for a fifo (persisted) **/
-//const mqttTopic = "fifo/dev_payload"
-
-/** connect **/
-console.log("MQTT::Connecting to ");
-var client  = mqtt.connect(url, {username:"payload", password:apiKey, keepAlive:30})
-
-/** client on connect **/
-client.on("connect", function() {
-  console.log("MQTT::Connected");
-
-  client.subscribe(mqttTopic)
-  console.log("MQTT::Subscribed to topic:", mqttTopic);
-})
-
-/** client on error **/
-client.on("error", function(err) {
-  console.log("MQTT::Error from client --> ", err);
-})
-
-client.on("message", function (topic, message) {
-
-  console.log("MQTT::New message\n");
-  var loraMessage = JSON.parse(message)
-
-  console.log("DevEUI:", loraMessage.metadata.source.split(':')[2]);
-  console.log("Timestamp:", loraMessage.timestamp);
-  console.log("Port:", loraMessage.metadata.network.lora.port);
-  console.log("Fcnt:", loraMessage.metadata.network.lora.fcnt);
-  console.log("Payload:", loraMessage.value.payload, "\n");
-  fs.writeFile('/home/site/wwwroot/test',loraMessage.timestamp, function (err,data) {
-  if (err) {
-    return console.log(err);
+    req.on('end', function() {
+      console.log('Received body data:');
+      console.log(data.toString());
+    });
   }
-  console.log(data);
-  });
-})
+
+  res.writeHead(statusCode, {'Content-Type': 'text/plain'});
+  res.end();
+}
+
+console.log("Listening to port 9000");
+console.log("Returning status code " + statusCode.toString());
